45b9ce8333e3783d9b4ff4f33569b409cf4ad09b
Joshua Jones <155779659+joshlamou@users.noreply.github.com>
2024-03-04 18:43:24 +0000

    minitests

--- minitestteam.rb
+++ minitestteam.rb
@@ -1,13 +1,12 @@
 require 'minitest/autorun'
 require './testteam'
 
-class TestTeam< Minitest::Test
+class TestTeam < Minitest::Test
   def setup
     @teamm = team.new
   end
-end
+
 
   def test_team_spirit
     assert_equal "Hoorah!", @team.team_spirit
   end
-end
